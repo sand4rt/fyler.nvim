8dc2faac84e15c6b7ecc18277b10f9ebd3640ea3
A7Lavinraj <lavinrajmohan@gmail.com>
2025-05-20 19:39:45 +0530

    feat: Implement `text` class

---
+++ lua/fyler/lib/text.lua
@@ -0,0 +1,90 @@
+---@alias Fyler.Text.Line { str: string, hl: string }
+
+---@class Fyler.Text.Options
+---@field left_margin? integer
+
+---@class Fyler.Text : Fyler.Text.Options
+---@field lines Fyler.Text.Line[][]
+local Text = {}
+
+---@param options? Fyler.Text.Options
+---@return Fyler.Text
+function Text.new(options)
+  return setmetatable({}, { __index = Text }):init(options)
+end
+
+---@param options? Fyler.Text.Options
+function Text:init(options)
+  options = options or {}
+  self.left_margin = options.left_margin or 0
+  self.lines = { {} }
+  return self
+end
+
+---@param count integer|nil
+function Text:nl(count)
+  for _ = 1, (count or 1) do
+    table.insert(self.lines, {})
+  end
+
+  return self
+end
+
+---@param str string
+---@param hl string
+function Text:append(str, hl)
+  table.insert(self.lines[#self.lines], { str = str, hl = hl })
+  return self
+end
+
+---@param bufnr integer
+function Text:render(bufnr)
+  local ns = require('fyler.config').values.namespace
+  local start_line = 0
+
+  if not (bufnr and vim.api.nvim_buf_is_valid(bufnr)) then
+    return
+  end
+
+  local was_modifiable = vim.bo[bufnr].modifiable
+
+  if not was_modifiable then
+    vim.bo[bufnr].modifiable = true
+  end
+
+  local virt_lines = {}
+
+  for _, line in ipairs(self.lines) do
+    local text = string.rep(' ', self.left_margin)
+
+    for _, segment in ipairs(line) do
+      text = text .. segment.str
+    end
+
+    table.insert(virt_lines, text)
+  end
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, virt_lines)
+  vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+
+  for i, line in ipairs(self.lines) do
+    local col = self.left_margin
+
+    for _, segment in ipairs(line) do
+      if segment.hl and segment.hl ~= '' then
+        vim.api.nvim_buf_set_extmark(bufnr, ns, start_line + i - 1, col, {
+          end_col = col + #segment.str,
+          hl_group = segment.hl,
+        })
+      end
+
+      col = col + #segment.str
+    end
+  end
+
+  if not was_modifiable then
+    vim.bo[bufnr].modifiable = false
+  end
+end
+
+return Text
