cae6d7547ce5a7d9a7d16dc3e965fd12976ba72e
A7Lavinraj <lavinrajmohan@gmail.com>
2025-05-20 19:40:08 +0530

    feat: Implement `window` class

---
+++ lua/fyler/lib/window.lua
@@ -0,0 +1,29 @@
+---@class Fyler.Window.Options
+---@field width number
+---@field split "top"|"right"|"bottom"|"left"
+---@field enter boolean
+
+---@class Fyler.Window : Fyler.Window.Options
+---@field bufnr? integer
+---@field winid? integer
+local Window = {}
+
+---@param options Fyler.Window.Options
+---@return Fyler.Window
+function Window.new(options)
+  return setmetatable({}, {
+    __index = Window,
+  }):init(options)
+end
+
+---@param options Fyler.Window.Options
+---@return Fyler.Window
+function Window:init(options)
+  for k, v in pairs(options or {}) do
+    self[k] = v
+  end
+
+  return self
+end
+
+return Window
