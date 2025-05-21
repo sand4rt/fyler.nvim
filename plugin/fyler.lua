eb015500b4dbabce7b94b631d291225e55bee89e
A7Lavinraj <lavinrajmohan@gmail.com>
2025-05-21 23:56:25 +0530

    feat: Add `highlights` on plugin load

---
+++ plugin/fyler.lua
@@ -0,0 +1,17 @@
+if vim.g.loaded_fyler == 1 then
+  return
+end
+vim.g.loaded_fyler = 1
+
+local highlights = {
+  FylerNormal = { default = true, link = 'Normal' },
+  FylerBorder = { default = true, link = 'FloatBorder' },
+  FylerTitle = { default = true, link = 'Directory' },
+  FylerSuccess = { default = true, link = 'String' },
+  FylerFailure = { default = true, link = 'Error' },
+  FylerWarning = { default = true, link = 'Constant' },
+}
+
+for k, v in pairs(highlights) do
+  vim.api.nvim_set_hl(0, k, v)
+end
