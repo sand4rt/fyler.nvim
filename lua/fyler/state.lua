5b9c57e6d73f3d93881778353e16c6cbdba9d226
A7Lavinraj <lavinrajmohan@gmail.com>
2025-05-25 01:53:38 +0530

    feat(state): Use collection approach

--- lua/fyler/state.lua
+++ lua/fyler/state.lua
@@ -1,12 +1,32 @@
-local state = {}
-local HashTable = {}
+local Collection = {}
 
-function state.set_key(key, value)
-  HashTable[key] = value
+function Collection.new()
+  return setmetatable({}, { __index = Collection })
 end
 
-function state.get_key(key)
-  return HashTable[key]
+---@param key string
+function Collection:get(key)
+  return self[key]
 end
 
-return state
+---@param key string
+---@param value string
+function Collection:set(key, value)
+  self[key] = value
+end
+
+---@class Fyler.State
+---@field collections table<string, table<string, any>>
+local State = setmetatable({ collections = {} }, {
+  __call = function(tbl, collection_name)
+    if tbl.collections[collection_name] then
+      return tbl.collections[collection_name]
+    end
+
+    tbl.collections[collection_name] = Collection.new()
+
+    return tbl.collections[collection_name]
+  end,
+})
+
+return State
