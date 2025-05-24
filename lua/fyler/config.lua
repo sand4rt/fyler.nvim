c2146f6fcf8fc9b91d574887b491a1f49065b5fe
A7Lavinraj <lavinrajmohan@gmail.com>
2025-05-25 01:55:55 +0530

    feat(config): Add `namespaces`

--- lua/fyler/config.lua
+++ lua/fyler/config.lua
@@ -2,7 +2,10 @@ local config = {}
 
 function config.set_defaults()
   config.values = {}
-  config.values.namespace = vim.api.nvim_create_namespace 'Fyler'
+  config.values.namespace = {
+    highlights = vim.api.nvim_create_namespace 'Fyler-highlights',
+  }
+  config.values.augroup = vim.api.nvim_create_augroup('Fyler', { clear = true })
 end
 
 return config
