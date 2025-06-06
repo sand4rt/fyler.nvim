d18caa068ca660937505dd0ba700057181fb7af7
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-06 13:01:02 +0530

    refactor!: Change default values and position of hijacking netrw

--- lua/fyler/init.lua
+++ lua/fyler/init.lua
@@ -117,6 +117,52 @@ end
 
 function M.setup(options)
   config.set_defaults(options)
+  if config.values.default_explorer then
+    local netrw_bufname
+    -- Clear FileExplorer autocmds to prevent netrw from launching
+    pcall(vim.api.nvim_clear_autocmds, { group = 'FileExplorer' })
+    -- Safety: Also clear on VimEnter
+    vim.api.nvim_create_autocmd('VimEnter', {
+      pattern = '*',
+      once = true,
+      callback = function()
+        pcall(vim.api.nvim_clear_autocmds, { group = 'FileExplorer' })
+      end,
+    })
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('FylerHijackNetrw', { clear = true }),
+      pattern = '*',
+      callback = function()
+        vim.schedule(function()
+          -- Don't hijack if already in netrw
+          if vim.bo[0].filetype == 'netrw' then
+            return
+          end
+
+          local bufname = vim.api.nvim_buf_get_name(0)
+          if vim.fn.isdirectory(bufname) == 0 then
+            local _, netrw_buf = pcall(vim.fn.expand, '#:p:h')
+            netrw_bufname = netrw_buf or ''
+
+            return
+          end
+
+          if netrw_bufname == bufname then
+            netrw_bufname = nil
+            return
+          else
+            netrw_bufname = bufname
+          end
+
+          -- Wipe the buffer so you don't leave a dummy buffer open
+          vim.api.nvim_buf_delete(0, {})
+          -- Launch plugin
+          require('fyler').show()
+        end)
+      end,
+      desc = 'fyler.nvim replacement for netrw',
+    })
+  end
 end
 
 vim.api.nvim_create_user_command('Fyler', M.show, { nargs = 0 })
