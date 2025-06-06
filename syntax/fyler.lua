415447edc3bc37c5687a2ba3ee47098925e672bf
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-06 17:15:15 +0530

    refactor: Convert vim syntax file to lua equivalent

---
+++ syntax/fyler.lua
@@ -0,0 +1,9 @@
+vim.cmd [[
+  if exists("b:current_syntax")
+  finish
+  endif
+
+  syntax match FylerMetaKey / \/\d*$/ conceal
+
+  let b:current_syntax = 'fyler'
+]]
--- syntax/fyler.vim
+++
@@ -1,7 +0,0 @@
-if exists("b:current_syntax")
-  finish
-endif
-
-syntax match FylerMetaKey / \/\d*$/ conceal
-
-let b:current_syntax = 'fyler'
