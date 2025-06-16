44792b5fa7a5d22fb7c8a9c2abf468cde01bcce2
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-16 17:14:47 +0530

    feat: Add notifiers to logger

--- lua/fyler/logger.lua
+++ lua/fyler/logger.lua
@@ -1,13 +1,31 @@
 local can_log = vim.env.FYLER_LOG_ENABLE or false
 local logger = {}
 
+local notify = vim.notify
+local levels = vim.log.levels
+
 ---@param args any
-logger.debug = function(args)
+function logger.debug(args)
   if not can_log then
     return
   end
 
   print(vim.inspect(args))
 end
+
+---@param msg string
+logger.info = vim.schedule_wrap(function(msg)
+  notify(msg, levels.INFO, { title = "Fyler.nvim" })
+end)
+
+---@param msg string
+logger.warn = vim.schedule_wrap(function(msg)
+  notify(msg, levels.WARN, { title = "Fyler.nvim" })
+end)
+
+---@param msg string
+logger.error = vim.schedule_wrap(function(msg)
+  notify(msg, levels.ERROR, { title = "Fyler.nvim" })
+end)
 
 return logger
