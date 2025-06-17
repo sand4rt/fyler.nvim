7000cc02e1eacfd0a1a6cb9ef7b322762e973dbc
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-17 13:00:20 +0530

    feat: Implement other necessary algorithms

--- lua/fyler/lib/algorithms/finder.lua
+++ lua/fyler/lib/algorithms/finder.lua
@@ -3,8 +3,8 @@ local M = {}
 -- Note: List items must have implementations of `__lt` and `__eq`
 ---@param list any[]
 ---@param item any
----@return integer
-function M.binary_search(list, item)
+---@return integer|nil
+function M.bs(list, item)
   local lb, ub = 1, #list
 
   while lb <= ub do
@@ -19,7 +19,51 @@ function M.binary_search(list, item)
     end
   end
 
-  return lb
+  return nil
+end
+
+-- Note: List items must have implementations of `__lt` and `__eq`
+---@param list any[]
+---@param item any
+---@return integer
+function M.ilb(list, item)
+  local lb, ub = 1, #list
+  local ans = #list + 1
+
+  while lb <= ub do
+    local mid = math.floor((lb + ub) * 0.5)
+
+    if list[mid] >= item then
+      ans = mid
+      ub = mid - 1
+    else
+      lb = mid + 1
+    end
+  end
+
+  return ans
+end
+
+-- Note: List items must have implementations of `__lt` and `__eq`
+---@param list any[]
+---@param item any
+---@return integer
+function M.iub(list, item)
+  local lb, ub = 1, #list
+  local ans = #list + 1
+
+  while lb <= ub do
+    local mid = math.floor((lb + ub) * 0.5)
+
+    if list[mid] > item then
+      ans = mid
+      ub = mid - 1
+    else
+      lb = mid + 1
+    end
+  end
+
+  return ans
 end
 
 return M
