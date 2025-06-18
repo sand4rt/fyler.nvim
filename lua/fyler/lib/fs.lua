e289cc1357955ed5f1fe7267afb136f06248b9ac
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-18 21:28:49 +0530

    feat: Update the file system module from previous codebase

--- lua/fyler/lib/fs.lua
+++ lua/fyler/lib/fs.lua
@@ -31,25 +31,6 @@ function M.toabspath(path)
 end
 
 ---@param path string
----@param callback fun(err?: string)
-function M.touch(path, callback)
-  uv.fs_stat(path, function(_, stats)
-    if stats then
-      callback("Item already exists")
-    else
-      uv.fs_open(path, "a", 420, function(o_err, fd)
-        if o_err then
-          callback(o_err)
-        else
-          assert(fd, "fd is a nil")
-          uv.fs_close(fd, callback)
-        end
-      end)
-    end
-  end)
-end
-
----@param path string
 ---@return table, string?
 function M.listdir(path)
   assert(path, "path is required")
@@ -74,6 +55,73 @@ function M.listdir(path)
   end
 
   return items, nil
+end
+
+---@param path string
+function M.create_fs_item(path)
+  local stat = uv.fs_stat(path)
+  if stat then
+    return
+  end
+
+  local path_type = string.sub(path, -1) == "/" and "directory" or "file"
+  if path_type == "directory" then
+    if vim.fn.mkdir(path, "p") == 0 then
+      return
+    end
+  else
+    local parent_path = vim.fn.fnamemodify(path, ":h")
+    if vim.fn.isdirectory(path) == 0 then
+      if vim.fn.mkdir(parent_path, "p") == 0 then
+        return
+      end
+    end
+
+    local fd, err = uv.fs_open(path, "w", 438)
+    if not fd or err then
+      return
+    end
+
+    uv.fs_close(fd)
+  end
+
+  vim.notify("CREATE: " .. path, vim.log.levels.INFO)
+end
+
+---@param path string
+function M.delete_fs_item(path)
+  local stat = uv.fs_stat(path)
+  if not stat then
+    return
+  end
+
+  if stat.type == "directory" then
+    vim.fn.delete(path, "rf")
+  elseif stat.type == "file" then
+    vim.fn.delete(path)
+  else
+    return
+  end
+
+  vim.notify("DELETE: " .. path, vim.log.levels.INFO)
+end
+
+---@param from string
+---@param to string
+function M.move_fs_item(from, to)
+  local from_stat = uv.fs_stat(from)
+  if not from_stat then
+    return
+  end
+
+  local parent_dir = vim.fn.fnamemodify(to, ":h")
+  local parent_stat = uv.fs_stat(parent_dir)
+  if not parent_stat then
+    M.create_fs_item(parent_dir .. "/")
+  end
+
+  uv.fs_rename(from, to)
+  vim.notify("MOVE: " .. from .. " > " .. to, vim.log.levels.INFO)
 end
 
 return M
