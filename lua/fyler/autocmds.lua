543278beabbf6c0bafe0420659f671065acaf65c
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-19 12:00:13 +0530

    feat: Capture `e` command to open directories

--- lua/fyler/autocmds.lua
+++ lua/fyler/autocmds.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local api = vim.api
+local uv = vim.uv or vim.loop
 
 function M.setup()
   api.nvim_create_autocmd("ColorScheme", {
@@ -28,6 +29,21 @@ function M.setup()
 
       vim.bo[arg.buf].filetype = "fyler"
       vim.bo[arg.buf].syntax = "fyler"
+    end,
+  })
+
+  api.nvim_create_autocmd("BufEnter", {
+    group = require("fyler").augroup,
+    callback = function(arg)
+      local stats = uv.fs_stat(arg.file)
+      if stats and stats.type == "directory" then
+        local cur_buf = api.nvim_get_current_buf()
+        if api.nvim_buf_is_valid(cur_buf) then
+          api.nvim_buf_delete(cur_buf, { force = true })
+        end
+
+        require("fyler").open { cwd = arg.file }
+      end
     end,
   })
 end
