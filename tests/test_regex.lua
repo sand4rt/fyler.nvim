6ceebce0fd59067043189781b61c786364a21fc3
A7Lavinraj <lavinrajmohan@gmail.com>
2025-06-23 21:20:09 +0530

    refactor: Using line by line tests instead of iteration

--- tests/test_regex.lua
+++ tests/test_regex.lua
@@ -3,46 +3,16 @@ local T = test.new_set()
 
 T["can extract key"] = function()
   local regex = require("fyler.views.file_tree.regex")
-  local tests = {
-    { str = "foobar.txt", exp = nil },
-    { str = "foobar.txt /1", exp = 1 },
-    { str = " foobar.txt /1", exp = 1 },
-  }
-
-  for _, item in ipairs(tests) do
-    test.expect.equality(regex.getkey(item.str), item.exp)
-  end
+  test.expect.equality(regex.getkey("foobar.txt"), nil)
+  test.expect.equality(regex.getkey("foobar.txt /1"), 1)
+  test.expect.equality(regex.getkey(" foobar.txt /1"), 1)
 end
 
 T["can extract name"] = function()
   local regex = require("fyler.views.file_tree.regex")
-  local tests = {
-    { str = "foobar.txt", exp = "foobar.txt" },
-    { str = "foobar.txt /1", exp = "foobar.txt" },
-
-    { str = " foobar.txt /1", exp = "foobar.txt" },
-
-    { str = "  foobar.txt  ", exp = "foobar.txt" },
-    { str = "    foobar.txt  /1  ", exp = "foobar.txt" },
-    { str = string.rep(" ", math.random(1, 5)) .. "foobar.txt", exp = "foobar.txt" },
-    {
-      str = string.rep(" ", math.random(1, 5)) .. "foobar.txt" .. string.rep(" ", math.random(1, 5)),
-      exp = "foobar.txt",
-    },
-    {
-      str = string.rep(" ", math.random(1, 5)) .. " foobar.txt" .. string.rep(" ", math.random(1, 5)),
-      exp = "foobar.txt",
-    },
-    { str = string.rep(" ", math.random(1, 5)) .. "foobar.txt /1", exp = "foobar.txt" },
-    {
-      str = string.rep(" ", math.random(1, 5)) .. " foobar.txt /1" .. string.rep(" ", math.random(1, 5)),
-      exp = "foobar.txt",
-    },
-  }
-
-  for _, item in ipairs(tests) do
-    test.expect.equality(regex.getname(item.str), item.exp)
-  end
+  test.expect.equality(regex.getname("foobar.txt"), "foobar.txt")
+  test.expect.equality(regex.getname("foobar.txt /1"), "foobar.txt")
+  test.expect.equality(regex.getname(" foobar.txt /1"), "foobar.txt")
 end
 
 return T
