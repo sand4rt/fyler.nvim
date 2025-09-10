d53e4fc184d4475943c7706c4e27032440577d05
A7Lavinraj <lavinrajmohan@gmail.com>
2025-09-10 16:32:19 +0530

    refactor(ui_components): Remove unnecessary code

--- lua/fyler/ui_components.lua
+++ lua/fyler/ui_components.lua
@@ -1,35 +1,6 @@
 local UiComponent = require "fyler.lib.ui.component"
 local M = {}
 
----@alias UiLineAlign
----| "end"
----| "start"
----| "center"
-
----@alias UiWord { str: string, hl: string|nil }
----@alias UiMark { str: string, hl: string|nil, id: string }
-
----@class UiLine
----@field align UiLineAlign
----@field words UiWord[]
----@field marks UiMark[]
-M.Line = {}
-M.Line.__index = M.Line
-
----@param opts { words: UiWord[], marks: UiMark[], align: UiLineAlign|nil }
----@return UiLine
-function M.Line.new(opts)
-  local instance = {
-    words = opts.words or {},
-    align = opts.align or "start",
-    marks = opts.marks or {},
-  }
-
-  setmetatable(instance, M.Line)
-
-  return instance
-end
-
 ---@param children UiComponent[]
 M.Column = UiComponent.new(function(children)
   return {
